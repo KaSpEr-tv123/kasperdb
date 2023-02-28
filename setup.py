f8fa0aefa1e038c52dbeb951eca62cae6ac58495
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-02-28 20:27:02 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kasperdb',
+      version='1.7',
+      description='Simple database for your project. Local database!!!',
+      packages=['kasperdb'],
+      author="KASPERENOK",
+      author_email='dimakukic1@gmail.com',
+      zip_safe=False)
